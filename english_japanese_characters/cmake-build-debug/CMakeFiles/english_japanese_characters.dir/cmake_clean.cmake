b1ab41864591520f54c99a256fba49bb3e312cc1
Eddie Tapia <Eddie Tapia>
2017-03-21 03:44:19 -0700

    added the code for english/japanese character question

---
+++ english_japanese_characters/cmake-build-debug/CMakeFiles/english_japanese_characters.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/english_japanese_characters.dir/main.cpp.o"
+  "english_japanese_characters.pdb"
+  "english_japanese_characters.exe"
+  "libenglish_japanese_characters.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/english_japanese_characters.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ english_japanese_characters/main.cpp
@@ -0,0 +1,92 @@
+/*
+ * Filename: main.cpp
+ * Author: Eddie Tapia
+ * Description: This file is used to check if the
+ * Date: 3/20/17
+ */
+
+#include <iostream>
+using namespace std;
+
+enum Result {
+    Japanese = 2,
+    English = 1,
+    Malformed = 0
+};
+struct jap_char{
+    char character[2]; // Two bytes
+
+    jap_char(char letter){
+        // Set the high bit
+        character[0] = (1 << 7);
+        // Set the low bit
+        character[1] = letter;
+    }
+};
+
+/*
+ * Function: NumChars
+ * Purpose: To determine whether the last character in the buffer defined
+ * as the bytes between begin and end is English or Japanese. Japanese
+ * characters are two bytes long and always have a 1 in the high bit of
+ * high byte. English chars are one byte long and always have a 0 in the
+ * high bit.
+ * Return: 2 if last character is Japanese
+ * 1 if last character is English
+ * 0 if the data in the buffer is malformed
+ */
+int NumChars(char * begin, char * end){
+    Result res = Malformed;
+    // Check beginning parameter is valid
+    if (!begin){
+        cout << "Invalid Parameters Passed Into NumChars" << endl;
+        return Malformed;
+    }
+    char *copyStart = begin;
+    bool flag = true;
+    while(flag) {
+        if (copyStart == end){
+            flag = false;
+        }
+        // int japaneseMask = 128;
+
+        // Check if the byte is a japanese character
+        int highbit = (*copyStart >> 7) & 1;
+
+        if (highbit == 1) {
+            // Check if there exists another char after it
+            if(copyStart == end){
+                return Malformed; // Since it is jap char, it needs two bytes
+            }
+            // Check if the last byte is the last character in the buffer
+            if( ++copyStart == end) {
+                flag = false;
+                res = Japanese;
+            }
+            else {
+                // If it is not the last character, keep traversing the buffer
+                copyStart += 2;
+            }
+        }
+        // Check if the byte is an english character
+        else if (highbit == 0) {
+            res = English;
+            copyStart++;
+        }
+    }
+    return res;
+}
+
+int main() {
+    char englishWord[] = "car";
+    char * englishEnd = englishWord + 2;
+    jap_char japaneseWord('s');
+    char * japCharPointer = japaneseWord.character;
+    char * japEndPointer = japaneseWord.character + 1;
+
+    int answer = NumChars(englishWord, englishEnd);
+    cout << answer << endl;
+    answer = NumChars(japCharPointer, japEndPointer);
+    cout << answer << endl;
+    return 0;
+}
